bb29658ddbccc99fc7bcad461564e46ced27bd2a
jplrez <joaozinhobent@gmail.com>
2019-11-20 17:50:55 -0300

    Place configuration constants in a separate file

---
+++ js/constants.js
@@ -0,0 +1,16 @@
+const FIELD_OF_VIEW = 90;   // in degrees
+
+const BACKGROUND_COLOR = "black";
+const AXES_COLOR = "white";
+const GRAPH_COLOR = "red";
+
+const CAMERA_POSITION = [0, 5, 10];
+
+// width and depth of the cuboid that contains the graph
+const WINDOW_DIMENSIONS = [50, 50];
+
+const WINDOW_XZ_RESOLUTION = [40, 40];
+
+const GRAPH_MORPH_TIME = 0.5;   // in seconds
+
+const AVOID_USELESS_REDRAWING = true;   // messes up zooming but saves energy
--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// Configurable constants.   TODO: move these to a separate module and reorganize them there.
-const FIELD_OF_VIEW = 90;   // in degrees
-const BACKGROUND_COLOR = "black";
-const AXES_COLOR = "white";
-const GRAPH_COLOR = "red";
-const CAMERA_POSITION = [0, 5, 10];
-// width and depth of the cuboid that contains the graph
-const WINDOW_DIMENSIONS = [10, 10];
-const WINDOW_XZ_RESOLUTION = [40, 40];
-const GRAPH_MORPH_TIME = 0.5;   // in seconds
-const AVOID_USELESS_REDRAWING = true;   // messes up zooming but saves energy
-
 let scene, camera, renderer;
 let surface;
 let keepRedrawing;
