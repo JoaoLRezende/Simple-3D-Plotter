d9177d8536ad900d48b16d67a4fe053a3145d0eb
jplrez <joaozinhobent@gmail.com>
2019-12-10 17:40:50 -0300

    Separate the JavaScript code into two files

---
+++ js/main_loop.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/* Declared in init.js but also used in this file:
+ * scene, camera, renderer, surface, keepRedrawing.
+ */
+
+
+function updateScene(time) {
+    time /= 1000;
+
+    /* If updateScene.newFunction is not null, then it holds
+     * a function that shall become the new target function. Start
+     * morphing the graph towards it.
+     */
+    if (updateScene.newFunction) {
+        updateScene.oldFunction = updateScene.targetFunction;
+        updateScene.targetFunction = updateScene.newFunction;
+        updateScene.morphStartTime = time;
+        updateScene.newFunction = null;
+    }
+
+    let progress = Math.min(1, (time - updateScene.morphStartTime) / GRAPH_MORPH_TIME);
+
+    surface.geometry.dispose();
+    let paramFunction = function(u, v, position) {
+        let x = -WINDOW_DIMENSIONS[0]/2 + u*(WINDOW_DIMENSIONS[0]);
+        let z = -WINDOW_DIMENSIONS[1]/2 + v*(WINDOW_DIMENSIONS[1]);
+        
+        let y = (1-progress)*updateScene.oldFunction(-x, -z)
+                +  progress *updateScene.targetFunction(-x, -z);
+
+        position.set(x, -y, z);
+    };
+    surface.geometry = new THREE.ParametricBufferGeometry(paramFunction, ...WINDOW_XZ_RESOLUTION);
+
+    renderer.render(scene, camera);
+
+    if (progress < 1 || keepRedrawing)
+        requestAnimationFrame(updateScene);
+}
+
+function drawNewFunction(e) {
+    let f;
+
+    try {
+        f = eval("(x,z) => (" + e.target.value + ")");
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+
+    updateScene.newFunction = f;
+    requestAnimationFrame(updateScene);
+}
