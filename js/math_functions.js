16def3f18805e9b4304f645fda207d9e81ef9949
jplrez <joaozinhobent@gmail.com>
2019-12-10 19:31:22 -0300

    Create common-function aliases to make typing easier

---
+++ js/math_functions.js
@@ -0,0 +1,18 @@
+/* Some function aliases to be used in the function input box.
+ */
+
+abs = Math.abs
+
+sin = sen = Math.sin
+cos = Math.cos
+tan = Math.tan
+
+E = Math.E
+exp = Math.exp
+log = Math.log
+
+sign = Math.sign
+
+sqrt = Math.sqrt
+
+pi = PI = Math.PI
