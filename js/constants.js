c7a6fd9fc0a421eecbc979e1e4193576ecf6db85
jplrez <joaozinhobent@gmail.com>
2019-12-10 18:09:51 -0300

    Increase the graph's resolution a little

--- js/constants.js
+++ js/constants.js
@@ -7,9 +7,9 @@ const GRAPH_COLOR = "red";
 const CAMERA_POSITION = [0, 3, 10];
 
 // width and depth of the cuboid that contains the graph
-const WINDOW_DIMENSIONS = [10, 10];
+const WINDOW_DIMENSIONS = [40, 10];
 
-const WINDOW_XZ_RESOLUTION = [40, 40];
+const WINDOW_XZ_RESOLUTION = [100, 30];
 
 const GRAPH_MORPH_TIME = 0.5;   // in seconds
 
